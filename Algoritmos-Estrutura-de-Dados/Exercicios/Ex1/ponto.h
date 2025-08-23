bf96559883cf8f93baeff10c3b5ce95ac29ba913
SofiaLima <albuquerque.sofialima@gmail.com>
2025-08-23 00:55:24 -0300

    organizacao de pastas

---
+++ Algoritmos-Estrutura-de-Dados/Exercicios/Ex1/ponto.h
@@ -0,0 +1,19 @@
+#ifndef PONTO_H
+    #define PONTO_H
+	#include <stdbool.h>
+	
+    typedef struct ponto_ PONTO;
+    PONTO *ponto_criar(float x, float y);
+    void ponto_apagar(PONTO *p);
+    bool ponto_set(PONTO *p, float x, float y);
+
+    /*
+    Pode ser necessário definir outras operações para o TAD.
+    Se for o caso, faça e justifique!
+    */
+#endif
+
+
+
+
+
